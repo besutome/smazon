43d3481b2f83e83f33f9009df154ea155bf85dbe
Kazunari.NAKATA <o9o64456583@gmail.com>
2015-10-11 12:41:05 +0900

    add react

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
 smazon
 =================
 
-- [ ] タイムセールを始めとした、セール商品一覧の表示
-- [ ] setting.jsをserverにもたせる
-- [ ] アカウント機能と通知機能
-- [ ] メールでの通知 http://justmeteor.com/blog/using-mailgun-with-meteor/
-
-
-```sh
-$ meteor add mquandalle:jade
-$ meteor add juliancwriko:s-jeet
-```
-
-```sh
-$ meteor add meteor add czbaker:apac
-$ meteor add meteorhacks:npm
-$ meteor --settings config/settigs.json
-```
-
+smazon.info
 
---
+++ smazon.jsx
@@ -0,0 +1,43 @@
+if (Meteor.isClient) {
+  Meteor.startup(function () {
+    React.render(<App />, document.getElementById("render-target"));
+
+  // // counter starts at 0
+  // Session.setDefault('counter', 0);
+  //
+  // Template.hello.helpers({
+  //   counter: function () {
+  //     return Session.get('counter');
+  //   }
+  // });
+  //
+  // Template.hello.events({
+  //   'click button': function () {
+  //     // increment the counter when button is clicked
+  //     Session.set('counter', Session.get('counter') + 1);
+  //   }
+  // });
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+
+    var aws = Meteor.npmRequire('aws-lib');
+    var amazon = Meteor.settings.amazon;
+
+    var prodAdv = aws.createProdAdvClient(amazon.id, amazon.secret, amazon.assocId, options);
+    var options = {SearchIndex: "Books", Keywords: "Javascript", host: "ecs.amazonaws.jp"};
+
+
+    prodAdv.call("ItemSearch", options, function(err, results) {
+      console.log(results.Items);
+    });
+
+
+
+  });
+}
+
+
+
