ef04f0c40035eb51eea43e37aa59386081b91e1b
Kazunari.NAKATA <o9o64456583@gmail.com>
2015-10-25 23:51:36 +0900

    use react

--- smazon.jsx
+++ smazon.jsx
@@ -4,22 +4,6 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Meteor.startup(function () {
     React.render(<App />, document.getElementById("render-target"));
-
-  // // counter starts at 0
-  // Session.setDefault('counter', 0);
-  //
-  // Template.hello.helpers({
-  //   counter: function () {
-  //     return Session.get('counter');
-  //   }
-  // });
-  //
-  // Template.hello.events({
-  //   'click button': function () {
-  //     // increment the counter when button is clicked
-  //     Session.set('counter', Session.get('counter') + 1);
-  //   }
-  // });
   });
 }
 
